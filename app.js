ad3922fe944e7337c36a893142aed000d8137807
AmyChen159 <144455940+AmyChen159@users.noreply.github.com>
2023-10-03 09:21:50 -0400

    qwq
    TT

---
+++ app.js
@@ -0,0 +1,12 @@
+function add(x, y) {
+    //creates an output
+    return x + y;
+}
+const sum = add(12,13);
+console.log(sum);
+
+function calc(){
+    const sum = add(1, 12);
+    console.log(sum);
+}
+calc();
